1a0a6f399b6d3b4dbbc95a63bf5df4053b82d3bf
Sudip <sudipdasghosh64@gmail.com>
2024-08-03 23:52:21 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,24 @@
+var menu = document.querySelector(".nav ul");
+var icon = document.querySelector("#toggle");
+var body = document.querySelector("body")
+
+var a = 0;
+icon.addEventListener("click", ()=>{
+  if(a == 0){
+    icon.innerHTML = `<i class="fa-solid fa-chevron-up"></i>`;
+    menu.style.top = "128px";
+    a = 1;
+  }else {
+    icon.innerHTML = `<i class="fa-solid fa-chevron-down"></i>`;
+    menu.style.top = "-150px";
+    a = 0;
+  }
+});
+
+
+
+
+
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault()
+}, false);
